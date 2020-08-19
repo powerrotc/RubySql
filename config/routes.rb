9f90557c9ca528947a50ffb75b81503d69c05e2f
powerrotc <powerrotc@gmail.com>
2020-08-19 04:00:57 +0000

    게시판만들기완성

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
     
-
-  get 'home/create'
-  get 'home/read'
-  get 'home/update'
-  get 'home/delete'
-  get 'home/create_form'
-  get 'home/update_form'
+  get  '/' => 'home#index'
+  post 'home/create'
+  get  'home/read'
+  post 'home/update/:id' => 'home#update' 
+  get  'home/delete/:id' => 'home#delete'
+  get  'home/create_form'
+  get  'home/update_form/:id' => 'home#update_form'
+  
+  post 'reply/create/:article_id'    => 'home#reply_create'
+  get  'reply/delete/:reply_id'      => 'home#reply_delete'  
+  get  'reply/update_form/:reply_id' => 'home#reply_update_form'   
+  post 'reply/update/:reply_id'      => 'home#reply_update'   
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_142232) do
+ActiveRecord::Schema.define(version: 2020_08_18_160413) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "author"
+    t.text "content"
+  end
+
+  create_table "replies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "article_id"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "UserID"
+    t.text "UserItem"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
